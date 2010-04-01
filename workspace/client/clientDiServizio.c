43526c20dd682ac05882ff02de459827166f2ef8
Alex82rm <Alex82rm@d621cd1a-8817-11de-8f56-7b2e0289298d>
2010-04-01 11:54:04 +0000

    Server e client riescono a scambiarsi solamente dati piccoli

--- workspace/client/clientDiServizio.c
+++ workspace/client/clientDiServizio.c
@@ -11,6 +11,7 @@ main() {
 	const char IP_ADDRESS[] = "127.0.0.1";	
 	struct sockaddr_in servaddr;
 	struct pacchetto pacchettoApplicativo;
+	char *cartellaDoveSalvareIfile = "fileCondivisi/";
 	
 	createSocketStream(&socketCl);
 	
@@ -66,25 +67,45 @@ main() {
 
 		bzero(&pacchettoApplicativo, sizeof(pacchettoApplicativo));
 		
-		numeroDatiRicevuti = receivePacchetto(&socketCl, &pacchettoApplicativo, sizeof(pacchettoApplicativo), 0);
+		receivePacchetto(&socketCl, &pacchettoApplicativo, sizeof(pacchettoApplicativo), 0);
 		
-		if(strcmp(pacchettoApplicativo.messaggio, "File trovato") == 0) {
-			strcat(pacchettoApplicativo.nomeFile, "_prova");
-			FILE *fileDaScrivere = fopen(pacchettoApplicativo.nomeFile, "wb");
+		if(strcmp(pacchettoApplicativo.tipoOperazione, "leggi file, trovato") == 0) {
 			
-			while(strcmp(pacchettoApplicativo.messaggio, "esci") != 0) {
+			char nomeFileDaScrivereConPercorso[sizeof(cartellaDoveSalvareIfile) + sizeof(pacchettoApplicativo.nomeFile)];
+			strcpy(nomeFileDaScrivereConPercorso, cartellaDoveSalvareIfile);
+			strcat(nomeFileDaScrivereConPercorso, pacchettoApplicativo.nomeFile);
+			
+			FILE *fileDaScrivere = fopen(nomeFileDaScrivereConPercorso, "wb");
+			
+			unsigned long int dimensioneDelFileRicevuto = atoi(pacchettoApplicativo.messaggio);
+			int numeroDiPartiCompleteDaRicevere = dimensioneDelFileRicevuto / (sizeof(pacchettoApplicativo.messaggio)-5);
+			unsigned long int numeroDiDatiScritti = 0;
+			
+			while(numeroDiPartiCompleteDaRicevere >= 0) {
 				bzero(&pacchettoApplicativo, sizeof(pacchettoApplicativo));
-				numeroDatiRicevuti = receivePacchetto(&socketCl, &pacchettoApplicativo, sizeof(pacchettoApplicativo), 0);
 				
-				fwrite(pacchettoApplicativo.messaggio, sizeof(char), sizeof(pacchettoApplicativo.messaggio), fileDaScrivere);
+				receivePacchetto(&socketCl, &pacchettoApplicativo, sizeof(pacchettoApplicativo), 0);
+				
+				if(numeroDiPartiCompleteDaRicevere != 0) {					
+					numeroDiDatiScritti = numeroDiDatiScritti + fwrite(pacchettoApplicativo.messaggio, 1, (sizeof(pacchettoApplicativo.messaggio)-5), fileDaScrivere);
+					printf("[%s] Scritti %ld dati.\n", pacchettoApplicativo.tipoOperazione, numeroDiDatiScritti);
+				}
+				
+				if(numeroDiPartiCompleteDaRicevere == 0) {
+					int dimensioneUltimaParteDaRicevere = dimensioneDelFileRicevuto % (sizeof(pacchettoApplicativo.messaggio)-5);
+					numeroDiDatiScritti = numeroDiDatiScritti + fwrite(pacchettoApplicativo.messaggio, 1, dimensioneUltimaParteDaRicevere, fileDaScrivere);
+					printf("[%s] Scritti %ld dati.\n", pacchettoApplicativo.tipoOperazione, numeroDiDatiScritti);
+				}
+				numeroDiPartiCompleteDaRicevere--;
 			}
+			
+			printf("[%s] Ho scritto %ld dati.\n", pacchettoApplicativo.tipoOperazione, numeroDiDatiScritti);
 			
 			fclose(fileDaScrivere);
 		}
 		
-		printf("Dati ricevuti: [%s] \n%s\n", pacchettoApplicativo.tipoOperazione, pacchettoApplicativo.messaggio);
-		
 		if(strcmp(pacchettoApplicativo.tipoOperazione, "Arrivederci") == 0) {
+			printf("[%s] %s\n", pacchettoApplicativo.tipoOperazione, pacchettoApplicativo.messaggio);
 			closeSocket(&socketCl);
 			break;
 		}
